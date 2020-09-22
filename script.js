76b5bd90221703c7e44233f0514db10d1893d5e4
Rakesh Chandra <rakesh.ragamalika@gmail.com>
2020-09-22 14:18:44 +1000

    git push

---
+++ script.js
@@ -0,0 +1,32 @@
+var search = $(".search");
+var input = $("#searchCity");
+
+search.on("submit", retrieve);
+
+
+function retrieve(e) {
+    var city = input.val();
+    console.log(city)
+    var apiKey = "34920371ed8ec37190ff37babcba6eb1"
+    var queryURl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
+
+    e.preventDefault();
+
+
+    $.ajax({
+        url: queryURl,
+        Method: "GET"
+    }).then(function (data) {
+        console.log(data);
+        var temperature = $("#temp");
+        var humid = $("#humidity");
+        var windSpeed = $("#wind");
+
+        $(temperature).text("Temperature(c): " + data.main.temp)
+        $(humid).text("Humidity(%): " + data.main.humidity)
+        $(windSpeed).text("Wind Speed(m/s): " + data.wind.speed)
+
+    })
+
+
+}
